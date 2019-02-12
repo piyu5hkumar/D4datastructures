a1811abea3d5730d332cc17741e46db0c499e61a
pyskmr <pyskmr@yahoo.com>
2019-02-12 23:23:06 +0530

    comments added

--- binary tree/insertion.cpp
+++ binary tree/insertion.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 template<class T>
-class node
+class node						// a base class is created which contain the data of every node in the tree
 {
 	public:
 	
@@ -23,7 +23,7 @@ node<T>::node()
 
 
 template<class S>
-class tree :public node<S>
+class tree :public node<S>		// tree is being made which contains a root for every tree object
 {
 	private:
 		
@@ -31,26 +31,27 @@ class tree :public node<S>
 
 	public:
 		tree();
-		void insert(S);
-		void preorder(node<S> *);
 		node<S>* getroot();
+		void insert(S);
+		void postorder(node<S> *);
+		
 
 };
 
 template<class S>
-tree<S>::tree()
+tree<S>::tree()							//as soon as the object will be made its root will be pointed to NULL
 {
 	root=NULL;
 }
 
 template<class S>
-node<S>* tree<S>::getroot()
+node<S>* tree<S>::getroot()				//this will give the pointer to the root
 {
 	return root;
 }
 
 template<class S>
-void tree <S>:: insert(S value)
+void tree <S>:: insert(S value)			//insertion part
 {
 	node<S> *temp = new node<S>;
 	temp->data = value;
@@ -60,56 +61,56 @@ void tree <S>:: insert(S value)
 	}
 	else
 	{
-		node<S> *current=root;
+		node<S> *current=root;					
 		node<S> *prev = NULL;
 		
 		while(current)
 		{
-			prev=current;
-			if(temp->data > current->data)
+			prev=current;						//this will store the last node visited because at the end the pointer will point to the null
+			if(temp->data > current->data)		// if the inserted data is greater than the available data in the node move to right sub tree
 			{
 				current=current->right;
 			}
 			else
 			{
-				current=current->left;
+				current=current->left;			// if the inserted data is greater than the available data in the node move to left sub tree
 			}
 		}
 
-		if (temp->data > prev->data)
+		if (temp->data > prev->data)			//last node is compared if the data is more make the new node as right child
 		{
 			prev->right = temp;
 		}
 		else
 		{
-			prev->left = temp;
+			prev->left = temp;					//if new node data is less make it left child
 		}
 
 	}
 } 
 
 template<class S>
-void tree<S>::preorder(node<S>* p)
+void tree<S>::postorder(node<S>* p)				//tree traversing using postorder approach
 {
 	if(p==NULL)
 	{
 		return ;
 	}
-	preorder(p->left);
-	preorder(p->right);
+	postorder(p->left);
+	postorder(p->right);
 	cout<<p->data<<"\t";
 }
 
 int main(int argc, char const *argv[])
 {
-	tree<int> obj;
-	obj.insert(5);
+	tree<int> obj;								//tree object
+	obj.insert(5);								//some initialisation.You can also insert by asking to the user but i did so to make the things more clear
 	obj.insert(2);
 	obj.insert(8);
 	obj.insert(4);
 	obj.insert(1);
 	obj.insert(7);
 	obj.insert(10);
-	obj.preorder(obj.getroot());
+	obj.postorder(obj.getroot());				//postorder traversel is being called in which the root of the tree is passed
 	return 0;
 }
